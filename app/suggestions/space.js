3459419e22a93888e2a37271df3e936abe2e2376
Joshua_Musyoki <musyokijoxhua@gmail.com>
2024-06-05 15:11:01 +0300

    Changes

---
+++ app/suggestions/space.js
@@ -0,0 +1,11 @@
+import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
+
+function Space() {
+    const position = {lat: 61.2176, lng: -149.8997}
+
+    return (
+        <APIProvider apiKey="">
+            
+        </APIProvider>
+    )
+}
